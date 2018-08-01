2b9da9f826781083dbc513a7f1caffcef298ff57
Richard Sanchez <richard.charles.s@gmail.com>
2018-08-01 19:24:57 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,9 @@
 class Artist
   attr_accessor :name, :song
-  #
-  # def initialize(name, song)
-  #   @name = name
-  #   @song = song
-  # end
+
+  def initialize(name, song)
+    @name = name
+    @song = song
+  end
 
 end
